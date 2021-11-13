b6767392a19a9b9039bd5a87408abbb41e57f1b3
Recep <50576891+RecepCelikk@users.noreply.github.com>
2021-11-13 13:34:13 +0300

    MvcProjeKamp-40

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator() //constructor metodu
+        {
+            RuleFor(x=>x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz.");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama Alanını Boş Geçemezsiniz.");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 Karakter Girişi Yapın.");
+            RuleFor(x => x.CategoryDescription).MaximumLength(20).WithMessage("Lütfen 20 Karakterden fazla değer girişi yapmayınız.");
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/IstatistikController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Context context = new Context();
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            var sonuc1 = context.Categories.Count();
+            var sonuc2 = context.Headings.Count(x => x.CategoryID== 10);
+            var sonuc3 = context.Writers.Count(x => x.WriterName.Contains("a"));
+            var sonuc4 = context.Headings.Max(x => x.Category.CategoryName);
+            var sonuc5 = context.Categories.Count(x => x.CategoryStatus == true);
+            var sonuc6 = context.Categories.Count(x => x.CategoryStatus == false);
+            ViewBag.CategoryCount = sonuc1;
+            ViewBag.Heading = sonuc2;
+            ViewBag.Writer = sonuc3;
+            ViewBag.HeadingMax = sonuc4;
+            ViewBag.StatusFark = (sonuc5 - sonuc6);
+
+            return View();
+        }
+    }
+}
